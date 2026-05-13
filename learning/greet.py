b05d1bbd74cb3af95ecdab2cb5add014155f0572
Art4v <aarav_20@outlook.com>
2026-05-13 20:44:25 +1000

    cli tool with argparse

--- learning/greet.py
+++ learning/greet.py
@@ -1,4 +1,22 @@
-import sys
+import argparse
 
-name = sys.argv[1] if len(sys.argv) > 1 else "world"
-print(f"Hello, {name}!")
+def main():
+
+    # define what arguements cli tool accepts
+    parser = argparse.ArgumentParser(description="Greet Someone") # creates the parser itself
+    parser.add_argument("name", help="who to greet") # declare a positional arguemnt 
+    parser.add_argument("--shout", action="store_true", help="greet loudly") # optional flag 
+    parser.add_argument("--times", type=int, default=1, help="how many times") # optional flag
+
+    # use arguements
+    args = parser.parse_args()
+
+    greeting = f"Hello, {args.name}!"
+    if args.shout:
+        greeting = greeting.upper()
+
+    for _ in range(args.times):
+        print(greeting)
+
+if __name__ == "__main__":
+    main()
