21268261eb5da5dfd291c612fef5d9d8e32dddb8
Art4v <aarav_20@outlook.com>
2026-05-14 22:05:49 +1000

    added to greet.py

--- learning/greet.py
+++ learning/greet.py
@@ -4,17 +4,23 @@ def main():
 
     # define what arguements cli tool accepts
     parser = argparse.ArgumentParser(description="Greet Someone") # creates the parser itself
-    parser.add_argument("name", help="who to greet") # declare a positional arguemnt 
+    parser.add_argument("first_name", help="first name") # declare a positional arguemnt 
+    parser.add_argument("last_name", help="last_name") # declare a positional arguement
+
     parser.add_argument("--shout", action="store_true", help="greet loudly") # optional flag 
     parser.add_argument("--times", type=int, default=1, help="how many times") # optional flag
+    parser.add_argument("--whisper", action="store_true", help="greet softly") # optional flag
 
     # use arguements
     args = parser.parse_args()
 
-    greeting = f"Hello, {args.name}!"
+    greeting = f"Hello, {args.first_name} {args.last_name}!"
     if args.shout:
         greeting = greeting.upper()
 
+    if args.whisper:
+        greeting = greeting.lower()
+    
     for _ in range(args.times):
         print(greeting)
 
